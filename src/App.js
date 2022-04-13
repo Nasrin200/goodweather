9f3e0bf73c23eb446e7d6e7317989428b096f92d
Nasrin200 <nasrin.sama@gmail.com>
2022-04-13 15:20:18 +0430

    added necessary files

--- src/App.js
+++ src/App.js
@@ -1,25 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
-
-function App() {
+import Weather from "./Weather";
+export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <Weather defaultCity="New York" />
+
+
+    <footer>This <a href='https://github.com/Nasrin200/weather-in-react' target="_blank" className="source">open source</a> app is made by Nasrin Alizadeh</footer>
+    </div>
     </div>
   );
 }
-
-export default App;
